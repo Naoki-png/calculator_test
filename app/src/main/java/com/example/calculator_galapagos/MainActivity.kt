7718ae169c2ccfbc57a108bd8f48bcad946cf0cb
Naoki-png <ct.anao38@outlook.jp>
2022-03-20 17:55:39 +0900

    レイアウト初期化処理を書いた

--- app/src/main/java/com/example/calculator_galapagos/MainActivity.kt
+++ app/src/main/java/com/example/calculator_galapagos/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.calculator_galapagos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.example.calculator_galapagos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        initButtons()
+    }
+
+    private fun initButtons() {
+        // 数値ボタン
+        initNumberButton(binding.zero)
+        initNumberButton(binding.one)
+        initNumberButton(binding.two)
+        initNumberButton(binding.three)
+        initNumberButton(binding.four)
+        initNumberButton(binding.five)
+        initNumberButton(binding.six)
+        initNumberButton(binding.seven)
+        initNumberButton(binding.eight)
+        initNumberButton(binding.nine)
+
+        // 計算ボタン
+        initCalcButton(binding.plus)
+        initCalcButton(binding.minus)
+        initCalcButton(binding.multiply)
+        initCalcButton(binding.divide)
+        initCalcButton(binding.equal)
+
+        // クリアボタン
+        initAllClearButton()
+    }
+
+    private fun initNumberButton(numberButton: TextView) {
+    }
+
+    private fun initCalcButton(calcButton: TextView) {
+    }
+
+    private fun initAllClearButton() {
     }
 }
